36c7ffae7ae1a84adcfcf87e099e22db05e51dc0
lee kinanga <lee254kinanga@gmail.com>
2024-03-06 08:57:41 +0300

    ui complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import "./styles.css"
 
 export default function App(){
-  return <form className="new-item-form">
+  return (
+    <>
+  <form className="new-item-form">
     <div classNme="form-row">
       <label htmlFor="item">New item</label>
       <input type="text" id="item" /> 
     </div>
-
+  <button className="btn">Add</button>
   </form>
+  <h1 className="header">To do List</h1>
+  <ul className="list">
+      <li>
+        <label>
+          <input type="checkbox" />
+          Learn react states
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+      <li>
+        <label>
+          <input type="checkbox" />
+          Learn appending children
+        </label>
+        <button className="btn btn-danger">Delete</button>
+      </li>
+  </ul>
+  </>
+  )
 }
